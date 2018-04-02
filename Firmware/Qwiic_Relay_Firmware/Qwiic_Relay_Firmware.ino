55ffe9f824ca679ab3ebeb21de99a484a45079e8
U-SPARKFUN\kevin.kuwata <kevin.kuwata@SX-KEVIN.sparkfun.local>
2018-04-02 13:40:59 -0600

    fixed bug when transfering from array based ISR handlers to 2 bytes command / address. the problem was i was servicing the ISR, and checking 2 checks for 1 byte of data, when in reality I need to do 1 check per 1 data, so then i needed else if and not 2 if's after eachother. the second if had 1 else statement that resulted in me dropping the second byte of data.

--- Firmware/Qwiic_Relay_Firmware/Qwiic_Relay_Firmware.ino
+++ Firmware/Qwiic_Relay_Firmware/Qwiic_Relay_Firmware.ino
@@ -15,15 +15,18 @@
 #define RELAY_PIN   4
 #define SETTING_LOCATION_ADDRESS	1
 
-const float  VERSION = 1.1;
+const float  version = 1.1;
  
 
 volatile byte qwiicRelayAddress  =    0x18; //default
 
 
-volatile int ReceivedData[2]; //2 byte array to act as a buffer for I2C data.  
 volatile byte command = 0x99;
 volatile byte address = 0x99;
+
+volatile byte versionFlag = 99; //this is the flag for flow control within the requestFrom ISR 
+					//determines if you are going to send status or version, changed in main after 
+					//receive 0x03 command. 
 
 void setup() {
 	
@@ -33,6 +36,7 @@ void setup() {
 		//EEPROM has never been written before, use the default address 0x18.
 		qwiicRelayAddress = 0x18; //default
 	}
+	
 
 	pinMode(RELAY_PIN, OUTPUT);
 	digitalWrite(RELAY_PIN, LOW);
@@ -43,33 +47,7 @@ void setup() {
 }
 
 void loop() {
-	
-	if(ReceivedData[0] == 0x01){
-		digitalWrite(RELAY_PIN, HIGH);
-		ReceivedData[0] = 0x99;
-	}
-	
-	if(ReceivedData[0] == 0x00){
-		digitalWrite(RELAY_PIN, LOW);
-		ReceivedData[0] = 0x99;
-	}
-	
-	if(ReceivedData[0] == 0x03){
-// check if valid address, if not valid , ignore.
-		if(address > 0x07 && address < 78){
-			//valid address, update and save to EEPROM
-						
-		qwiicRelayAddress = address;	
-		
-		//save to EEPROM
-		EEPROM.write(1, qwiicRelayAddress);
-		TinyWire.begin(qwiicRelayAddress);		
-		}			
-		ReceivedData[0] = 0x99;
-		ReceivedData[1] = 0x99;
-	}
-	
-	/*
+
 	if(command == 0x01){
 		digitalWrite(RELAY_PIN, HIGH);
 		command = 0x99;
@@ -95,11 +73,11 @@ void loop() {
 		command = 0x99;
 		address = 0x99;
 	}
-	*/
+	
 	
 	
 	if(command == 0x04 ){ //report back with firmware version
-	//	TinyWire.send(VERSION);
+		versionFlag = 1;
 	// set a flag so that the next time i do a request I sent 4 bytes (= float) to the master...
 	/*
 		should be similar to the get status but get status was built when we only sent back status
@@ -111,6 +89,14 @@ void loop() {
 		lets make sure the changes I did make work. 
 	*/
 	}
+	
+	/*
+	//TODO: this might be necessary and make the master look exactly the same
+	// structure as the getFrimwareVersion() in master 
+	if(command == 0x05){
+		versionFlag = 0;
+	}
+	*/
 	
 }
 
@@ -124,32 +110,21 @@ void loop() {
 
 void receiveEvent(int bytesReceived) {
 
-byte count = 0; //count of all the data received from master.
-
-  while(TinyWire.available() > 0){ 
-	
-	ReceivedData[count] = TinyWire.read();
-	count++;
- }
-
-/*	
 	byte count = 0; //count of all the data received from master.
 
   while(TinyWire.available() > 0){ 
 	if(count == 0){
-	command = TinyWire.read();}
-	
-	if(count == 1){
-	address = TinyWire.read();}
-	else{
-		TinyWire.read();
+	command = TinyWire.read();
 	}
-	count++;
 	
-  }//end of the while loop, data available
-  
-  
-  */
+	else if(count == 1){
+	address = TinyWire.read();
+	}
+	else{
+		TinyWire.read(); //read the data coming in but ignore it.
+  }
+  	count++;
+  }
 }// end of receive ISR
 
 
@@ -158,9 +133,30 @@ byte count = 0; //count of all the data received from master.
   //    ISR is triggered. 
 
 void onI2CRequest() {
-	//TODO: add in something to report the address.
-	if(digitalRead(RELAY_PIN) == HIGH) TinyWire.send(0x01);
-	else{
-		TinyWire.send(0x00);
+	
+	//not sure if this will work because the master sends 0x04, then the salve enters the RX event isr
+	// then goes to main, sets the flag. then if that happens slower than versionFlag we have a race condition. 
+	
+
+	if(versionFlag == 1){
+	TinyWire.send(version); //tiny.wire doesn't like to send multiple bytes so lets see how this goes. we can always_noconv
+	//make the version a number just 1 byte then convert over to TinyCore.
+	
+	versionFlag = 0; // reset the flag
 	}
+
+ else if(versionFlag == 0 ){ 
+	if(digitalRead(RELAY_PIN) == HIGH) TinyWire.send(0x01); 
+	else{
+		TinyWire.send(0x00); 
+	}
+	versionFlag = 99;
+}
+//put the else if inside so only 1 thing happens per ISR request. 
+
+	
+	/*
+		so now that we have the version number [float] we want to send we need to have some flow control.
+		
+	*/
 }// end of request ISR
